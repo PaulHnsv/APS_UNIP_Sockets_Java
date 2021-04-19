c7a343b46f87b971dec5fd1959d9211668ac2db8
PaulHnsv <65260913+PaulHnsv@users.noreply.github.com>
2021-04-19 16:26:00 -0300

    feat: thread para receber mensagens do server

--- src/main/ClienteTCPBasico.java
+++ src/main/ClienteTCPBasico.java
@@ -1,17 +1,46 @@
 package main;
 
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-import java.io.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import recebeMensagens.Recebedor;
 
 public class ClienteTCPBasico {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket cliente = new Socket("127.0.0.1", 12345);
+		// dispara cliente
+		new ClienteTCPBasico("127.0.0.1", 12345).executa();
+	}
+
+	private String host;
+	private int porta;
+
+	public ClienteTCPBasico(String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente = new Socket(this.host, this.porta);
 		System.out.println("O cliente se conectou ao servidor!");
 
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new Thread(r).start();
+
+		// lê msgs do teclado e manda pro servidor
 		Scanner teclado = new Scanner(System.in);
 		PrintStream saida = new PrintStream(cliente.getOutputStream());
-
 		while (teclado.hasNextLine()) {
 			saida.println(teclado.nextLine());
 		}
